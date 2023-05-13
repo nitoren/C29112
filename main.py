af198b25697590cf2d68b8a5c1c65dd5ca06f8d5
nitoren <bfox2080@gmail.com>
2023-05-13 11:46:36 +0300

    into

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+
+# help()
+def first_function():
+    pass
+
+
+class Human:
+    pass
+
+
+rq = requests
+first_f = first_function
+vasya = Human
+
+print(requests.__name__)
+print(rq.__name__)
+print(first_function.__name__)
+print(first_f.__name__)
+print(Human.__name__)
+print(vasya.__name__)
+
+
+
+
